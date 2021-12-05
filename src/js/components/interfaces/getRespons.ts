0f38f005a772a00879c694b5741d435d0c416047
Bam6ycha <84175555+Bam6ycha@users.noreply.github.com>
2021-12-05 17:12:06 +0300

    Delete src directory

--- src/js/components/interfaces/getRespons.ts
+++
@@ -1,6 +0,0 @@
-import { CustomOptions } from "./options";
-
-export interface getResponse {
-  endpoint: string;
-  options?: CustomOptions;
-}
--- src/js/main.ts
+++
@@ -1,9 +0,0 @@
-import "../index.html";
-import "../scss/main.scss";
-import { burgerMenu } from "./burgerMenu/BurgerMenu";
-
-import App from "./components/app/app";
-
-const app = new App();
-app.start();
-burgerMenu;
