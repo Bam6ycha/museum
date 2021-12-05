6a3eec94bc568ee62a78928039dc97c8b533725a
Dmitrii Kosarev <kosarev-d@mail.ru>
2021-12-05 17:44:44 +0300

    feat : add proxy server

--- src/js/components/controller/appLoader.ts
+++ src/js/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from "./loader";
 
 class AppLoader extends Loader {
   constructor() {
-    super("https://newsapi.org/v2/", {
+    super("https://nodenews.herokuapp.com/", {
       apiKey: "a8ac38dc4157435ba38c741838c8af19"
     });
   }
